bc915674a441ca76406b3c7eaaa57729c9abe6cc
Victor Hugo <111382980+NXrding-dev@users.noreply.github.com>
2023-12-21 01:48:57 -0300

    Cardápio Amdin

---
+++ View/cardapio-admin.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cardapio-admin.css">
+    <title>Administração de Cardápios</title>
+</head>
+<body>
+    <header>
+        <img src="../assets/1b1210fdf4454600bea220983da0cc63.png" alt="logo-ifba-seabra">
+    </header>
+
+    <?php include_once("navbar.php"); showNav("default"); ?>
+
+    <div class="container">
+        <h1>Administração de Cardápios</h1><br>
+
+        <div class="filtro-container">
+            <h2>Filtrar</h2>
+            <div class="filtro-campos">
+                <label class="label-data" for="filtro-data-inicio">Data início:</label>
+                <input type="date" id="filtro-data-inicio" placeholder="Filtrar por Data Início">
+
+                <label class="label-data" for="filtro-data-fim">Data fim:</label>
+                <input type="date" id="filtro-data-fim" placeholder="Filtrar por Data Fim">
+            </div>
+        </div>
+
+        <div class="cardapios">
+            <!-- Tabela de Cardápios -->
+            <table>
+                <thead>
+                    <tr>
+                        <th>Data Início</th>
+                        <th>Data Fim</th>
+                        <th>Ações</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <!-- Exemplo de linha na tabela, substitua com dados reais do banco de dados -->
+                    <tr>
+                        <td>01/01/2023</td>
+                        <td>05/01/2023</td>
+                        <td>
+                            <button onclick="editarCardapio(1)">Editar</button>
+                            <button onclick="excluirCardapio(1)">Excluir</button>
+                        </td>
+                        <td><input type="checkbox" name="selecao" value="1"></td>
+                    </tr>
+                    <!-- Adicione mais linhas conforme necessário -->
+                </tbody>
+            </table>
+
+            <!-- Botões para Adicionar e Limpar Seleção -->
+            <div class="botoes-admin">
+                <a href="cardapio-criar.php" class="botao-adicionar">Adicionar Cardápio</a>
+                <button onclick="limparSelecao()">Limpar Seleção</button>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div>
+            <img src="../assets/1b1210fdf4454600bea220983da0cc63.png" alt="logo-ifba-seabra" class="logo">
+        </div>
+        <div class="copyright">
+          <p>&copy; 2023 | IFBA - Instituto Federal de Educação, Ciência e Tecnologia da Bahia
+            Campus Seabra</p>
+        </div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ View/cardapio-criar.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cardapio-criar.css">
+    <title>Cardápio ADMIN</title>
+</head>
+<body>
+    <header>
+        <img src="../assets/1b1210fdf4454600bea220983da0cc63.png" alt="logo-ifba-seabra">
+    </header>
+
+    <?php include_once("navbar.php"); showNav("default"); ?>
+
+    <div class="container">
+        <h1>CRIAR CARDÁPIO</h1>
+        <form id="cardapioForm">
+            <label for="data-inicio">Data inicío:</label>
+            <input type="date" id="data-inicio" name="data-inicio" required>
+
+            <label for="data-fim">Data fim:</label>
+            <input type="date" id="data-fim" name="data-fim" required>
+
+            <div class="dia-semana">
+                <div>
+                    <label for="segunda">Segunda-feira:</label>
+                    <input type="text" id="segunda" name="segunda" required>
+                </div>
+                <div>
+                    <label for="acompanhamento-segunda">Acompanhamento:</label>
+                    <input type="text" id="acompanhamento-segunda" name="acompanhamento-segunda" required>
+                </div>
+                <div>
+                    <label for="sobremesa-segunda">Sobremesa:</label>
+                    <input type="text" id="sobremesa-segunda" name="sobremesa-segunda" required>
+                </div>
+            </div>
+
+            <div class="dia-semana">
+                <div>
+                    <label for="terca">Terça-feira:</label>
+                    <input type="text" id="terca" name="terca" required>
+                </div>
+                <div>
+                    <label for="acompanhamento-terca">Acompanhamento:</label>
+                    <input type="text" id="acompanhamento-terca" name="acompanhamento-terca" required>
+                </div>
+                <div>
+                    <label for="sobremesa-terca">Sobremesa:</label>
+                    <input type="text" id="sobremesa-terca" name="sobremesa-terca" required>
+                </div>
+            </div>
+
+            <div class="dia-semana">
+                <div>
+                    <label for="quarta">Quarta-feira:</label>
+                    <input type="text" id="quarta" name="quarta" required>
+                </div>
+                <div>
+                    <label for="acompanhamento-quarta">Acompanhamento:</label>
+                    <input type="text" id="acompanhamento-quarta" name="acompanhamento-quarta" required>
+                </div>
+                <div>
+                    <label for="sobremesa-quarta">Sobremesa:</label>
+                    <input type="text" id="sobremesa-quarta" name="sobremesa-quarta" required>
+                </div>
+            </div>
+
+            <div class="dia-semana">
+                <div>
+                    <label for="quinta">Quinta-feira:</label>
+                    <input type="text" id="quinta" name="quinta" required>
+                </div>
+                <div>
+                    <label for="acompanhamento-quinta">Acompanhamento:</label>
+                    <input type="text" id="acompanhamento-quinta" name="acompanhamento-quinta" required>
+                </div>
+                <div>
+                    <label for="sobremesa-quinta">Sobremesa:</label>
+                    <input type="text" id="sobremesa-quinta" name="sobremesa-quinta" required>
+                </div>
+            </div>
+
+            <div class="dia-semana">
+                <div>
+                    <label for="sexta">Sexta-feira:</label>
+                    <input type="text" id="sexta" name="sexta" required>
+                </div>
+                <div>
+                    <label for="acompanhamento-sexta">Acompanhamento:</label>
+                    <input type="text" id="acompanhamento-sexta" name="acompanhamento-sexta" required>
+                </div>
+                <div>
+                    <label for="sobremesa-sexta">Sobremesa:</label>
+                    <input type="text" id="sobremesa-sexta" name="sobremesa-sexta" required>
+                </div>
+            </div>
+
+            <div class="botao-container">
+                <button type="button" onclick="validarCardapio()">Validar Cardápio</button>
+                <button type="button" onclick="cancelarCardapio()">Cancelar Cardápio</button>
+            </div>
+        </form>
+    </div>
+
+    <footer>
+        <div>
+            <img src="../assets/1b1210fdf4454600bea220983da0cc63.png" alt="logo-ifba-seabra" class="logo">
+        </div>
+        <div class="copyright">
+          <p>&copy; 2023 | IFBA - Instituto Federal de Educação, Ciência e Tecnologia da Bahia Campus Seabra</p>
+        </div>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
